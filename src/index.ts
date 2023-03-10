f10044300270e9ed958303aaae2a77d2dcf41c6a
Vishaal MK <vishaalmk@pivotx.co>
2023-03-10 14:52:40 +0530

    Added README

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import * as TranscriptionListenerHelpers from './transcripts_listener';
-import * as SymblTranscriptionsHelpers from './bhasa_transcriptions';
+import * as BhasaTranscriptionsHelpers from './bhasa_transcriptions';
 import {
     ActivateTranscriptionsConfig,
     DeactivateTranscriptionsConfig,
@@ -12,19 +12,19 @@ async function activateTranscriptions(param: ActivateTranscriptionsConfig) {
         throw new Error('arguments[0].meeting.self is not available. Did you miss calling new DyteClient first?');
     }
     if (!param?.bhasaAccessToken) {
-        throw new Error('Missing arguments[0].bhasaAccessToken. We need Bhasa access token to retrive conversations and to generate transcriptions');
+        throw new Error('Missing arguments[0].bhasaAccessToken. Provide Bhasa access token');
     }
     if (!param?.languageCode) {
-        throw new Error('language code is missing');
+        throw new Error('Language code is missing');
     }
-    return SymblTranscriptionsHelpers.activateTranscriptions(param);
+    return BhasaTranscriptionsHelpers.activateTranscriptions(param);
 }
 
 async function deactivateTranscriptions(param: DeactivateTranscriptionsConfig) {
     if (!param.meeting?.self) {
         throw new Error('arguments[0].meeting.self is not available. Did you miss calling new DyteClient first?');
     }
-    return SymblTranscriptionsHelpers.deactivateTranscriptions(param);
+    return BhasaTranscriptionsHelpers.deactivateTranscriptions(param);
 }
 
 async function addTranscriptionsListerner(param: AddTranscriptionsListenerConfig) {
